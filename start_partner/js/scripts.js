fae0f588eb335bdecec743a4f62907ab70d67bfd
Volodymyr Petruliak <106393188+snuber-w@users.noreply.github.com>
2024-03-18 09:36:58 +0200

    upd

--- start_partner/js/scripts.js
+++ start_partner/js/scripts.js
@@ -1,19 +1,21 @@
-if (document.getElementById("menu")) {
-  const menuBtn = document.getElementById("menu");
-  const closeMenuBtn = document.getElementById("menu-close");
+const menuBtn = document.getElementById("menu");
+const closeMenuBtn = document.getElementById("menu-close");
 
-  console.log(menuBtn);
+if (menuBtn && closeMenuBtn) {
+  menuBtn.addEventListener('click', handleMenuOpen);
+  closeMenuBtn.addEventListener('click', handleMenuClose);
+}
 
-  menuBtn.addEventListener('click', () => {
-    event.preventDefault();
+function handleMenuOpen(event) {
+  event.preventDefault();
+  const burgerMenu = document.querySelector(".burger__menu");
+  burgerMenu.style.right = "0";
+  burgerMenu.style.left = "0";
+}
 
-    document.querySelector(".burger__menu").style.right = 0;
-    document.querySelector(".burger__menu").style.left = 0;
-  });
-  closeMenuBtn.addEventListener('click', () => {
-    event.preventDefault();
-
-    document.querySelector(".burger__menu").style.right = "150vw";
-    document.querySelector(".burger__menu").style.left = "-150vw";
-  });
+function handleMenuClose(event) {
+  event.preventDefault();
+  const burgerMenu = document.querySelector(".burger__menu");
+  burgerMenu.style.right = "200vw";
+  burgerMenu.style.left = "-150vw";
 }
