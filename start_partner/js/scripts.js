1ea2bb3a8132b561641240ab186211213f85a954
Volodymyr Petruliak <106393188+snuber-w@users.noreply.github.com>
2024-03-19 16:50:29 +0200

    upd

--- start_partner/js/scripts.js
+++ start_partner/js/scripts.js
@@ -32,19 +32,10 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    // if (panel.style.display === "block") {
-    //   panel.style.display = "none";
-    // } else {
-    //   panel.style.display = "block";
-    // }
-
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-      panel.style.overflow = "auto";
-      panel.style.height = "auto";
-      panel.style.opacity = "1";
     }
   });
 }
